2c929c51566a45b198bd82077dc57d4369641833
Vincent_tt <evgeniy161102@gmail.com>
2025-02-03 15:06:20 +0300

    Added header adaptation for mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import { NotFoundPage } from "./pages/NotFound/NotFoundPage";
-import { HomePage } from "./pages/Home/HomePage";
+// import { HomePage } from "./pages/Home/HomePage";
 
 /* 
   FIXME: Images from https://cheboksary.stores-apple.com/
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="*" element={<NotFoundPage />} />
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
       </Routes>
       <Footer />
     </main>
