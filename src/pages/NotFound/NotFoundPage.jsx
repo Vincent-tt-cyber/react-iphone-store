3a85280932a515e7b3fc013485fce57030c6f45b
Vincent_tt <evgeniy161102@gmail.com>
2025-02-13 13:09:12 +0300

    Добавлена анимация для страницы 404

--- src/pages/NotFound/NotFoundPage.jsx
+++ src/pages/NotFound/NotFoundPage.jsx
@@ -7,6 +7,9 @@ export const NotFoundPage = () => {
       <div className={styles["not-found"]}>
         <div className="container">
           <div className={styles["not-found__content"]}>
+            <div className={styles["not-found__content__wrap"]}>
+              <div className={styles["eye"]}></div>
+            </div>
             <h1>Страница не найдена</h1>
             <Link to="/">Вернуться на главную</Link>
           </div>
